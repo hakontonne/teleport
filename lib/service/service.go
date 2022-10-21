d3e84672fe7fe28a65836eab34a4e8a7b089585c
Gavin Frazar <gavin.frazar@goteleport.com>
2022-10-21 12:39:37 -0700

    Rename ok->shouldSkipCleanup to make the purpose of it more clear

--- lib/service/service.go
+++ lib/service/service.go
@@ -4298,9 +4298,9 @@ func (process *TeleportProcess) initApps() {
 			return trace.Wrap(err)
 		}
 
-		ok := false
+		shouldSkipCleanup := false
 		defer func() {
-			if !ok {
+			if !shouldSkipCleanup {
 				warnOnErr(conn.Close(), log)
 			}
 		}()
@@ -4435,7 +4435,7 @@ func (process *TeleportProcess) initApps() {
 		}
 		defer func() {
 			if !shouldSkipCleanup {
-				warnOnErr(asyncEmitter.Close(), process.log)
+				warnOnErr(asyncEmitter.Close(), log)
 			}
 		}()
 
@@ -4461,7 +4461,7 @@ func (process *TeleportProcess) initApps() {
 		}
 
 		defer func() {
-			if !ok {
+			if !shouldSkipCleanup {
 				warnOnErr(appServer.Close(), log)
 			}
 		}()
@@ -4499,8 +4499,8 @@ func (process *TeleportProcess) initApps() {
 		log.Infof("All applications successfully started.")
 
 		// Cancel deferred cleanup actions, because we're going
-		// to regsiter an OnExit handler to take care of it
-		ok = true
+		// to register an OnExit handler to take care of it
+		shouldSkipCleanup = true
 
 		// Execute this when process is asked to exit.
 		process.OnExit("apps.stop", func(payload interface{}) {
